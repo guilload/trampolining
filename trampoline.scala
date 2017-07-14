5b90685ba186840624907656667f165a15bc8879
Adrien Guillo <adrien.guillo@gmail.com>
2017-07-14 15:12:10 -0400

    Add `call` and `done` constructors

--- trampoline.scala
+++ trampoline.scala
@@ -3,6 +3,13 @@ import scala.annotation.tailrec
 
 sealed trait Trampoline[+A]
 
+object Trampoline {
+
+    def call[A](expr: => Trampoline[A]): Call[A] = Call(() => expr)
+    def done[A](value: A): Done[A] = Done(value)
+
+}
+
 case class Call[A](thunk: () => Trampoline[A]) extends Trampoline[A]
 case class Done[A](value: A) extends Trampoline[A]
 
@@ -16,13 +23,13 @@ def trampoline[A](trmpln: Trampoline[A]): A = {
 }
 
 def trampolineIsEven(n: Int): Trampoline[Boolean] = {
-    if (n == 0) Done(true)
-    else Call(() => trampolineIsOdd(n - 1))
+    if (n == 0) Trampoline.done(true)
+    else Trampoline.call(trampolineIsOdd(n - 1))
 }
 
 def trampolineIsOdd(n: Int): Trampoline[Boolean] = {
-    if (n == 0) Done(false)
-    else Call(() => trampolineIsEven(n - 1))
+    if (n == 0) Trampoline.done(false)
+    else Trampoline.call(trampolineIsEven(n - 1))
 }
 
 def IsEven(n: Int): Boolean = {
